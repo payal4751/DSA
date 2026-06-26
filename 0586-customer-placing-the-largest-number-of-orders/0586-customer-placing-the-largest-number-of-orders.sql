b117e1a589ab36b17a493ac6a3090eb91e200eac
khuspepayal <khuspepayal2003@gmail.com>
2026-06-26 22:26:39 +0530

    Time: 405 ms (100%), Space: 0B (100%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select customer_number
+from orders
+group by customer_number
+order by count(*) desc
+limit 1;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
