056d4f21199a503c309ab04faec9500e4ec842ae
khuspepayal <khuspepayal2003@gmail.com>
2026-06-26 22:33:54 +0530

    Time: 493 ms (99.3%), Space: 0B (100%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT PRODUCT_ID FROM PRODUCTS
+WHERE LOW_FATS='Y' AND RECYCLABLE='Y';
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
