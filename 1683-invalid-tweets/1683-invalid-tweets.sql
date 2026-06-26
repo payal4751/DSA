a305faf8aa4a955e744cee3fcb1921276f9fa581
khuspepayal <khuspepayal2003@gmail.com>
2026-06-26 22:32:38 +0530

    Time: 567 ms (96.71%), Space: 0B (100%) - LeetHub - Time: O, Space: O(?)

---
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT TWEET_ID
+FROM TWEETS
+WHERE LENGTH(CONTENT)>15;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
