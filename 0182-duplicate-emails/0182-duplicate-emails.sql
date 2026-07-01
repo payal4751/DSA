187354cfc3437723936f10e379f07fd10f3456bd
payal4751 <khuspepayal2003@gmail.com>
2026-07-01 23:18:38 +0530

    Time: 599 ms (5%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
