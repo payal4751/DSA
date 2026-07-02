61b0cdd518f72c32b96c5d268cba6eae6266395e
payal4751 <khuspepayal2003@gmail.com>
2026-07-02 12:40:50 +0530

    Time: 16 ms (63.11%), Space: 47 MB (56.97%) - LeetHub - Time: O(m*n*log(m*n)), Space: O(m*n)

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+
+        int m = grid.size();
+        int n = grid.get(0).size();
+
+        int[][] dist = new int[m][n];
+
+        for (int[] row : dist)
+            Arrays.fill(row, Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq =
+                new PriorityQueue<>((a,b) -> a[2]-b[2]);
+
+        dist[0][0] = grid.get(0).get(0);
+        pq.offer(new int[]{0,0,dist[0][0]});
+
+        while(!pq.isEmpty()){
+
+            int[] cur = pq.poll();
+
+            int x = cur[0];
+            int y = cur[1];
+            int cost = cur[2];
+
+            if(cost > dist[x][y])
+                continue;
+
+            if(x==m-1 && y==n-1)
+                break;
+
+            for(int[] d:dir){
+
+                int nx=x+d[0];
+                int ny=y+d[1];
+
+                if(nx<0||ny<0||nx>=m||ny>=n)
+                    continue;
+
+                int newCost = cost + grid.get(nx).get(ny);
+
+                if(newCost < dist[nx][ny]){
+                    dist[nx][ny]=newCost;
+                    pq.offer(new int[]{nx,ny,newCost});
+                }
+            }
+        }
+
+        return dist[m-1][n-1] < health;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
