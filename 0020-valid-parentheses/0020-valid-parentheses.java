6610b08d01aca0b6cdb1f4b646b20ecea8904b52
payal4751 <khuspepayal2003@gmail.com>
2026-07-03 15:56:33 +0530

    Time: 163 ms (5.93%), Space: 47.2 MB (5.12%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
+        }
+
+        return s.isEmpty();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
