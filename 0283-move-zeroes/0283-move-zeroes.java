96f1c8cf99e809c083fd2f70dd8b8f3614e26171
payal4751 <khuspepayal2003@gmail.com>
2026-07-11 19:19:24 +0530

    Time: 2 ms (92.23%), Space: 47.9 MB (20.16%) - LeetHub - Time: O(n), Space: O(1)

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int slow=0;
+        int fast = 0;
 
-    for(int i=0;i<nums.length;i++){
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[i]==0 && nums[j]!=0){
-            nums[i]=nums[j];
-            nums[j]=0;
+    while (fast<nums.length) {
+
+        if (nums[fast]!=0) {
+
+            int temp = nums[fast];
+            nums[fast] = nums[slow];
+            nums[slow] = temp;
+            slow++;
         }
+            fast++;
+    
     }
     }
-}
 }
 
 // Synced seamlessly with LeetHub Pro
