8f67fb4565bfb43f6cf119005119dc93a20fb6ac
payal4751 <khuspepayal2003@gmail.com>
2026-07-16 18:30:07 +0530

    Time: 302 ms (53.12%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(1)

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT *,
+IF(x+y>z AND y+z>x AND x+z>y, "Yes","No") as Triangle
+FROM Triangle
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
