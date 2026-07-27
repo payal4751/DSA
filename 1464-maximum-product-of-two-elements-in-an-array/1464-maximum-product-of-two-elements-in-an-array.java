77f7d63603b2a8dd60a171eb25bc3a1b3d2b943b
payal4751 <khuspepayal2003@gmail.com>
2026-07-27 19:14:25 +0530

    Time: 0 ms (100%), Space: 44.4 MB (86.08%) - LeetHub - Time: O(n), Space: O(1)

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,15 +1,23 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max=Integer.MIN_VALUE;
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int prod=(nums[i]-1)*(nums[j]-1);
-                if(prod>max){
-                    max=prod;
-                }
+            if(nums[i]>=max1){
+                max2=max1;
+                max1=nums[i];
+            } else if(nums[i]>max2){
+                max2=nums[i];
             }
         }
-        return max;
+       
+        // Correct! Your approach efficiently finds the two largest elements in a single pass.
+        // Time Complexity: O(n) - where n is the length of the array.
+        // Space Complexity: O(1) - as you only use two integer variables.
+        // This is the optimal complexity for this problem.
+        // Feel free to submit your solution!
+        return (max1 - 1) * (max2 - 1);
     }
 }
 
