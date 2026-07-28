82af284a2a76f2b2b12d642190350fbd618bde27
payal4751 <khuspepayal2003@gmail.com>
2026-07-28 09:43:07 +0530

    Time: 33 ms (43.78%), Space: 48.1 MB (57.43%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        StringBuilder left = new StringBuilder();
+        char middle = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq[i] & 1) == 1) {
+                middle = (char) ('a' + i);
+            }
+
+            for (int j = 0; j < freq[i] / 2; j++) {
+                left.append((char) ('a' + i));
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        ans.append(left);
+
+        if (middle != 0) {
+            ans.append(middle);
+        }
+
+        ans.append(new StringBuilder(left).reverse());
+
+        return ans.toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
